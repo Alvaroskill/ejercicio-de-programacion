c82dce919b18bfa36d42adba254a466f0e80d333
Alvaroskill <allinfer22@gmail.com>
2021-10-18 20:09:52 +0200

    Ejercicios de lectura de datos

---
+++ Tema03/Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2{
+  public static void main(String[] args) {
+    System.out.println("Conversión de euros a pesetas");
+    System.out.println("Introduce el número de euros que quieres convertir:");
+    float Puesto = Float.parseFloat(System.console().readLine());
+    double euros;
+    euros = Puesto* 166.386;
+    System.out.printf("%.2f pesetas", euros);
+  }
+}
