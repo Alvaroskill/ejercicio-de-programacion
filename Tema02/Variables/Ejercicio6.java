2d534397376d7715d76ebb4ac5c5da243869869d
Alvaroskill <allinfer22@gmail.com>
2021-10-18 09:18:42 +0200

    Ejercicios de variables

--- Tema02/Variables/Ejercicio6.java
+++ Tema02/Variables/Ejercicio6.java
@@ -6,7 +6,7 @@ public class Ejercicio6 {
     PrecioSinIva = 45;
     Total = (PrecioSinIva*0.21) + PrecioSinIva;
    
-    System.out.println(" El precio del producto sin IVA sería " + PrecioSinIva);
-    System.out.println(" El precio del producto con IVA sería " + Total); 
+    System.out.println(" El precio del producto sin IVA sería " + PrecioSinIva + "€" );
+    System.out.println(" El precio del producto con IVA sería " + Total + "€"  	     ); 
   }
 }
