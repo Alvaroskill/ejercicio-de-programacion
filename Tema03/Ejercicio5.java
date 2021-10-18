39b2460b908064951fea74fdab171cb5bc205dc3
Alvaroskill <allinfer22@gmail.com>
2021-10-18 20:54:25 +0200

    Ejercicios de lectura de datos

--- Tema03/Ejercicio5.java
+++ Tema03/Ejercicio5.java
@@ -4,18 +4,18 @@ public class Ejercicio5 {
 
     String linea;
 
-    System.out.print("Por favor, introduzca la altura del triángulo: ");
+    System.out.print("Por favor, introduzca la altura del rectángulo: ");
     linea = System.console().readLine();
     int altura;
     altura = Integer.parseInt(linea);
 
-    System.out.print("Lo mismo, con la base del triangulo: ");
+    System.out.print("Lo mismo, con la base del rectángulo: ");
     linea = System.console().readLine();
     int base;
     base = Integer.parseInt(linea);
 
    int área; 
-   área = (altura*base)/2;
-    System.out.print("El área de este triángulo es de " + área +" metros cuadrados"); 
+   área = altura*base;
+    System.out.print("El área de este rectángulo es de " + área +" metros cuadrados"); 
   } 
 }
---
+++ Tema03/Ejercicio6.java
@@ -0,0 +1,21 @@
+public class Ejercicio6 {
+  public static void main(String[] args) {
+
+    String linea;
+
+    System.out.print("Por favor, introduzca la altura del triángulo: ");
+    linea = System.console().readLine();
+    int altura;
+    altura = Integer.parseInt(linea);
+
+    System.out.print("Lo mismo, con la base del triangulo: ");
+    linea = System.console().readLine();
+    int base;
+    base = Integer.parseInt(linea);
+
+   int área; 
+   área = (altura*base)/2;
+    System.out.print("El área de este triángulo es de " + área +" metros cuadrados"); 
+  } 
+}
+
