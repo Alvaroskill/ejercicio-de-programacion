72d4018b77a2d2f6a372cc5f0232fd2db57a3eb5
Alvaroskill <allinfer22@gmail.com>
2021-10-18 08:34:10 +0200

    Ejercicios de variables

--- Tema01/Variables/Ejercicio2.java
+++
@@ -1,8 +0,0 @@
-
-public class Ejercicio2 {
-  public static void main(String[] args) {
-    String nombre= "Álvaro Linero Fernández"; // Declara la variable nombre
-	// Muestra el nombre 
-    System.out.println("Me llamo " + nombre);
-  }
-}
