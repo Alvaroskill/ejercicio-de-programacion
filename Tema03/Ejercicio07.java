f4215f0c88b007c148b397af5cb14ec39e1ef541
Alvaroskill <allinfer22@gmail.com>
2021-10-21 08:48:05 +0200

    Ejercicios de lectura de datos

--- Tema03/Ejercicio07.java
+++ Tema03/Ejercicio07.java
@@ -1,5 +1,5 @@
 
-public class Ejercicio7 {
+public class Ejercicio07 {
   public static void main(String[] args) {
     System.out.print("Introduce el precio de la factura sin Iva: ");
     int PrecioSinIva = Integer.parseInt(System.console().readLine());
