5082c90303792feebeaa41b1553e6194e61abc50
Alvaroskill <allinfer22@gmail.com>
2021-10-21 18:06:46 +0200

    Ejercicios de Condicionales

---
+++ Tema04/Ejercicio01.java
@@ -0,0 +1,44 @@
+
+
+
+public class Ejercicio01 {
+  public static void main(String[] args) {
+  
+    System.out.println("Introduzca un día de la semana: ");
+    System.out.println("------------------------------- ");
+    System.out.println("1 Lunes"    );
+    System.out.println("2 Martes "  );
+    System.out.println("3 Miercoles");
+    System.out.println("4 Jueves"   );
+    System.out.println("5 Viernes " );
+    
+    int opcion = Integer.parseInt(System.console().readLine());
+    
+
+
+  switch (opcion) {
+      case 1:
+        System.out.print("Ha primera hora toca Programación");
+        break;
+    
+      case 2:
+        System.out.print ("Ha primera hora toca Programación");
+        break;
+    
+      case 3:
+        System.out.print ("Ha primera hora toca Sistemas Informáticos");
+        break;
+        
+      case 4:
+        System.out.print ("Ha primera hora toca Programación");
+        break;
+      
+      case 5:
+        System.out.print ("Ha primera hora toca Sistemas Informáticos");
+        break;
+        
+      default:
+        System.out.print("Lo sentimos es número no esta en la elección.");
+    }
+  }
+}
